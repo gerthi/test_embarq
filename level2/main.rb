43517380838f3b565c6cf756672d1983fb54c6a9
Thibaud Gerin <thibaud.gerin@gmail.com>
2022-01-12 12:27:09 +0100

    level2 done

---
+++ level2/main.rb
@@ -0,0 +1,23 @@
+require 'json'
+
+require_relative 'lib/Car'
+require_relative 'lib/Rental'
+
+input = JSON.parse(File.read('./data/input.json'))
+
+# creating the cars from the input data
+input.dig('cars').each do |data|
+    Car.new(data)
+end 
+
+# creating the rentals from the input data
+input.dig('rentals').each do |data|
+    Rental.new(data)
+end
+
+# generating the output file
+file_output = File.join(__dir__, './data/output.json')
+
+File.open(file_output, 'wb') do |file|
+    file.write(JSON.generate(Rental.generate_report))
+end
